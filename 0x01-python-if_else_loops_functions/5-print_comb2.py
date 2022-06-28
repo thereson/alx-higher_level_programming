57cb7ef4f5e38ad424de03eefc272d139184d701
thereson <gerardokolie09@gmail.com>
2022-06-28 16:43:59 -0400

    task 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for x in range(100):
+    if (x != 99):
+        print("{}{}, ".format(int(x / 10), x % 10), end="")
+    else:
+        print("{}{}".format(int(x / 10), x % 10))
